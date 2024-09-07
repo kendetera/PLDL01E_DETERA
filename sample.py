0284b4ba3786520e11ce8936d4bf28a526464d2c
Jstger <santiagogerald994@gmail.com>
2024-09-07 12:07:20 +0800

    PLD

---
+++ sample.py
@@ -0,0 +1,22 @@
+
+
+
+#initialization for the value and sum of product
+
+sum = 0
+product = 0
+
+#getting inputs for first number and second number value
+
+first_number = int(input("Enter the value of the 1st number: "))
+second_number = int(input("Enter the value of the 2nd number: "))
+
+#setting formula for the computation of sum and product
+
+sum = first_number + second_number
+product = first_number * second_number
+
+#displaying the computed value for sum and product
+
+print("the sum of two numbers is", sum)
+print("the product of two numbers", product)
