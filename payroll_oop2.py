6ac41c36b3ac92fedf1838fb70ca2241c830f2b5
kendetera <kendetera05@gmail.com>
2024-10-05 11:36:19 +0800

    PLD

--- payroll_oop2.py
+++ payroll_oop2.py
@@ -12,3 +12,52 @@ class Employee_Info:
         self.emp_name = emp_name
         self.emp_code = emp_code
         self.salary_cut_off = salary_cut_off
+
+    def display_data(self):
+        print("Company Name: ", self.company_name)
+        print("Employee Department: ", self.emp_department)
+        print("Employee Name: ", self.emp_name)
+        print("Employee Code: ", self.emp_code)
+        print("Cut-Off Date: ", self.salary_cut_off)
+
+
+class Employee_Salary:
+    def __init__(self):
+        self.sss_contribution = 0.00
+        self.hdmf_contribution = 100.00
+        self.philhealth_contribution = 0.00
+        self.tax_contribution = 0.00
+        self.emp_absences = 0.00
+        self.emp_tardiness = 0.00
+        self.total_deduction = 0.00
+        self.emp_rate_per_hour = 0.00
+        self.emp_num_of_hours_per_payday = 0.00
+        self.hour_overtime = 0.00
+        self.overtime_pay = 0.00
+        self.basic_pay = 0.00
+        self.honorarium_pay = 0.00
+        self.emp_num_of_absences = 0.00
+        self.tardiness_deduction = 0.00
+        self.tardiness_num_hours = 0.00
+        self.emp_gross_earnings = 0.00
+        self.emp_net_income = 0.00
+
+    def get_basic_pay(self, emp_rate_per_hour, emp_num_of_hours_per_payday):
+        self.basic_pay = emp_rate_per_hour * emp_num_of_hours_per_payday
+        return self.basic_pay
+
+    def get_absences_deduction(self, emp_rate_per_hour, emp_num_of_absences):
+        self.emp_absences = emp_rate_per_hour * emp_num_of_absences
+        return self.emp_absences
+
+    def get_overtime_pay(self, emp_rate_per_hour, emp_hour_overtime):
+        self.overtime_pay = emp_rate_per_hour * emp_hour_overtime
+        return self.overtime_pay
+
+    def get_tardiness_deduction(self, emp_rate_per_hour, emp_tardiness_num_hours):
+        self.tardiness_deduction = emp_rate_per_hour * emp_tardiness_num_hours
+        return self.tardiness_deduction
+
+    def get_total_deduction(self, sss_contribution, philhealth_contribution, tax_contribution, emp_absences, emp_tardiness):
+        self.total_deduction = sss_contribution + philhealth_contribution + tax_contribution + emp_absences + emp_tardiness
+        return self.total_deduction
