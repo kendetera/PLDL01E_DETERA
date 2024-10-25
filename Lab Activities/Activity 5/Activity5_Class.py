c1ca7afa07cb1bcb6de31b3efb3888b4d9df21bc
kendetera <kendetera05@gmail.com>
2024-10-25 10:02:26 +0800

    Update Activity5_Class.py

--- Lab Activities/Activity 5/Activity5_Class.py
+++ Lab Activities/Activity 5/Activity5_Class.py
@@ -58,7 +58,7 @@ class Assessment: # Class for assessment
         return self.total_assessment_amount() - self.downpayment
 
     def total_assessment_amount(self): # method to get the total assessment amount
-        return (self.get_tuition_fee() + self.adu_chronicle + self.athletic + self.audio_visual + self.ausg +
+        return (self.tuition_fee + self.adu_chronicle + self.athletic + self.audio_visual + self.ausg +
                 self.cultural_fee + self.energy_cost_aircon_classroom + self.guidance + self.insurance +
                 self.learning_management_system + self.library_fee + self.medical_and_dental + self.registration +
                 self.rso + self.student_activities_fee + self.student_nurturance_fee + self.technology_fee +
