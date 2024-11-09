178861bfe589ea0c199cc95a1352b38d3d467a6c
kendetera <kendetera05@gmail.com>
2024-11-09 12:10:43 +0800

    Midterm Quiz

---
+++ Lab Activities/Midterm Quiz/MidtermQ1.py
@@ -0,0 +1,66 @@
+class CustomerInfo:
+    def __init__(self):
+        # initialize  inputs
+        self.customer_name = input("Enter customer name: ")
+        self.customer_street_address = input("Enter customer street address: ")
+        self.customer_barangay_address = input("Enter customer barangay address: ")
+        self.customer_municipality = input("Enter customer municipality: ")
+        self.customer_city = input("Enter customer city: ")
+        self.customer_account_number = input("Enter customer account number: ")
+
+
+class ElectricBill:
+    def __init__(self):
+        self.amount_due = int(input("Enter amount due: "))
+        self.remaining_balance = int(input("Enter remaining balance: "))
+        self.total_amount_due = 0
+        self.previous_billing_info = ''
+
+    def get_previous_billing_info(self):
+        if self.remaining_balance == 0:
+            return "Thank You"
+        else:
+            return "Please pay your remaining balance"
+
+    def get_total_amount_due(self):  # calculate for the total amount due
+        self.total_amount_due = self.amount_due + self.remaining_balance
+        return self.total_amount_due
+
+
+class ServiceInfo:
+    def __init__(self, customer):
+        self.customer = customer
+        self.service_id_number = self.customer.customer_account_number
+        self.rate = input("Residential or Business: ")
+        self.contact_in_the_name_of = self.customer.customer_name
+        self.service_street_address = self.customer.customer_street_address
+        self.service_barangay_address = self.customer.customer_barangay_address
+        self.service_municipality = self.customer.customer_municipality
+        self.service_city = self.customer.customer_city
+
+
+class BillingInfo:
+    def __init__(self, bill):
+        self.bill = bill
+        self.bill_date = input("Enter Bill Date: ")
+        self.meter_reading_date = input("Enter meter reading date: ")
+        self.bill_period = input("Enter bill period: ")
+        self.due_date = input("Enter due date: ")
+        self.total_kwh = input("Enter Total KWH: ")
+        self.total_current_amount = self.bill.total_amount_due
+        self.next_meter_reading = input("Enter next meter reading date: ")
+
+
+class BillComputationSummary:
+    def __init__(self):
+        self.generation = input("Enter Generation charge: ")
+        self.transmission = input("Enter Transmission charge: ")
+        self.system_loss = input("Enter System Loss charge: ")
+        self.distribution = input("Enter Distribution (Meralco) charge: ")
+        self.subsides = input("Enter Subsides charge: ")
+        self.government_taxes = input("Enter Government Taxes charge: ")
+        self.universal_charges = input("Enter Universal charges: ")
+        self.fit_all = input("Enter FiT-All charge: ")
+        self.applied_credits = input("Enter Applied Credits charge: ")
+        self.other_charges = input("Enter Other charges: ")
+        self.installment_due = input("Enter Installment Due charge: ")
---
+++ Lab Activities/Midterm Quiz/MidtermQ1_A.py
@@ -0,0 +1,94 @@
+import MidtermQ1  # import the MidtermQ1 module
+
+
+def main():
+    # calling of our classes and their methods
+    customer_info = MidtermQ1.CustomerInfo()
+
+    electric_bill = MidtermQ1.ElectricBill()
+    electric_bill.get_previous_billing_info()
+    electric_bill.get_total_amount_due()
+
+    service_info = MidtermQ1.ServiceInfo(customer_info)
+
+    billing_info = MidtermQ1.BillingInfo(electric_bill)
+
+    bill_computation_summary = MidtermQ1.BillComputationSummary()
+
+    # call the display functions
+    display_customer_info(customer_info)
+    display_electric_bill(electric_bill)
+    display_service_info(service_info)
+    display_billing_info(billing_info)
+    display_bill_computation_summary(bill_computation_summary)
+
+
+def display_customer_info(customer_info):
+    print(f'''
+    {customer_info.customer_name}
+    {customer_info.customer_street_address}
+    {customer_info.customer_barangay_address}
+    {customer_info.customer_municipality}
+    {customer_info.customer_city}
+
+    ************************** 
+    ''')
+
+
+def display_electric_bill(electric_bill):
+    print(f'''
+    Balance From Previous Billing 
+    {electric_bill.remaining_balance} \t\t {electric_bill.get_previous_billing_info()}
+
+    Current Charges
+    {electric_bill.amount_due} \t\t {electric_bill.total_amount_due}
+
+    **************************
+    ''')
+
+
+def display_service_info(service_info):
+    print(f'''
+    Service ID Number: {service_info.service_id_number}
+    Rate: {service_info.rate}
+    Contact in the name of {service_info.contact_in_the_name_of}
+    Service Address: {service_info.service_street_address} {service_info.service_barangay_address} 
+                     {service_info.service_municipality} {service_info.service_city}
+
+    **************************
+    ''')
+
+
+def display_billing_info(billing_info):
+    print(f''''
+    Bill Date: {billing_info.bill_date}
+    Meter Reading Date: {billing_info.meter_reading_date}
+    Bill Period: {billing_info.bill_period}
+    Due Date: {billing_info.due_date}
+    Total KWH: {billing_info.total_kwh}    
+    Total Current Amount: {billing_info.total_current_amount}
+    Next Meter Reading: {billing_info.next_meter_reading}
+    
+    **************************
+    ''')
+
+
+def display_bill_computation_summary(bill_computation_summary):
+    print(f'''
+    Generation: {bill_computation_summary.generation}
+    Transmission: {bill_computation_summary.transmission}
+    System Loss: {bill_computation_summary.system_loss}
+    Distribution (Meralco): {bill_computation_summary.distribution}
+    Subsides: {bill_computation_summary.subsides}
+    Government Taxes: {bill_computation_summary.government_taxes}
+    Universal Charges: {bill_computation_summary.universal_charges}
+    FiT-All (Renewable): {bill_computation_summary.fit_all}
+    Applied Credits: {bill_computation_summary.applied_credits}
+    Other Charges: {bill_computation_summary.other_charges}
+    Installment Due: {bill_computation_summary.installment_due}
+
+    ''')
+
+
+if __name__ == '__main__':
+    main()
